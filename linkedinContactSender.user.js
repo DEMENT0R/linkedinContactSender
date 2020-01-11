206d45d73eaad12072db0566d07f86da13fdc35a
DEMENTOR <dmntr@rambler.ru>
2020-01-12 03:43:05 +1000

    add: recommended page

--- linkedinContactSender.user.js
+++ linkedinContactSender.user.js
@@ -3,14 +3,16 @@
 // @namespace    linkedinContactSender
 // @description  linkedin.com
 // @icon         https://static-exp1.licdn.com/sc/h/al2o9zrvru7aqj8e1x2rzsrca
-// @version      0.1.1
+// @version      0.1.2
 // @author       DEMENT0R
 // @downloadURL  https://github.com/DEMENT0R/linkedinContactSender/raw/master/linkedinContactSender.user.js
 // @updateURL    https://github.com/DEMENT0R/linkedinContactSender/raw/master/linkedinContactSender.user.js
 // @license      MIT
 // @noframes
 // @match        https://*.linkedin.cn/search*
+// @match        https://*.linkedin.cn/mynetwork/*
 // @match        https://*.linkedin.com/search*
+// @match        https://*.linkedin.com/mynetwork/*
 // @require      http://ajax.googleapis.com/ajax/libs/jquery/1.8.3/jquery.min.js
 // @grant        GM_addStyle
 // ==/UserScript==
@@ -21,6 +23,7 @@
     var $ = window.jQuery;
 
     setInterval(function(){
+        // search
         if($("button").is(":contains('Установить контакт')")){
             //alert($("button").is(":contains('Установить контакт')"));
             $("button:contains('Установить контакт')")[0].click();
@@ -28,7 +31,6 @@
         } else {
             $("button:contains('Далее')")[0].click();
         }
-
     }, 4000);
     function doAccept(){
         setTimeout(function(){
